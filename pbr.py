c065a5c6da53d8ce62237177a1fa5b0d34baead7
busitaro <busitaro10@gmail.com>
2020-08-10 16:18:09 +0900

    price.pyからPBR取得機能をpbr.pyに分離

--- pbr.py
+++ pbr.py
@@ -6,7 +6,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 base_url = 'https://kabutan.jp/stock/kabuka?code={code}'
-out_file = './{code}_{date}_price.csv'
+out_file = './pbr.csv'
 max_retry = 5
 sleep_time = 100
 
@@ -45,26 +45,23 @@ def is_valid_html(soup):
         return False
 
 
-def get_price_chart(soup):
-    for tr in soup.select('#stock_kabuka_table tr'):
-        td_list = list(map(lambda x: x.text, tr.find_all(['td', 'th'])))
-        yield *td_list[:4], *td_list[6:]
+def get_pbr(soup):
+    return soup.select('#stockinfo_i3 tr td')[1].text[:-1]
 
 
 def main():
-    today = datetime.date.today().strftime('%Y%m%d')
-    for code in range(1300, 10000):
-        time.sleep(sleep_time / 1000)
-        soup = get_html(code)
-        try:
-            if soup is not None:
-                with open(out_file.format(code=code, date=today), 'w') as f:
-                    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-                    for row in get_price_chart(soup):
-                        writer.writerow(row)
-        except Exception as e:
-            print('type:{}'.format(str(type(e))))
-            print('args:' + str(e.args))
+    with open(out_file, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for code in range(1300, 10000):
+            time.sleep(sleep_time / 1000)
+            print(code)
+            soup = get_html(code)
+            try:
+                if soup is not None:
+                    writer.writerow([code, get_pbr(soup)])
+            except Exception as e:
+                print('type:{}'.format(str(type(e))))
+                print('args:' + str(e.args))
 
 
 if __name__ == '__main__':
