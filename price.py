683d19f59bb97679492fc456b4844910e674b57d
busitaro <busitaro10@gmail.com>
2020-09-19 16:10:32 +0900

    price.pyの価格データを時系列の昇順に出力するように修正

--- price.py
+++ price.py
@@ -15,24 +15,27 @@ def get_price_chart_html(code):
 
 
 def get_price_chart(soup):
-    for tr in soup.select('#stock_kabuka_table tr')[3:]:
-        td_list = list(map(lambda x: x.text, tr.find_all(['td', 'th'])))
-        yield td_list
+    tr_list = soup.select('#stock_kabuka_table tr')[3:]
+    td_list = [list(map(lambda x: x.text, tr.find_all(['td', 'th']))) for tr in tr_list]
+    return td_list
 
 
 def main(from_code=1300):
     for code in range(from_code, 10000):
         time.sleep(sleep_time / 1000)
+        out_data = []
         for soup in get_price_chart_html(code):
             try:
                 if soup is not None:
-                    with open(out_file.format(code=code), 'a') as f:
-                        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-                        for row in get_price_chart(soup):
-                            writer.writerow(row)
+                    out_data.extend(get_price_chart(soup))
             except Exception as e:
                 print('type:{}'.format(str(type(e))))
                 print('args:' + str(e.args))
+        if len(out_data) > 0:
+            out_data = sorted(out_data, key=lambda x: x[0])
+            with open(out_file.format(code=code), 'a', encoding='utf_8', newline='') as f:
+                writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+                writer.writerows(out_data)
 
 
 if __name__ == '__main__':
