4f7ab1a71354debc1cb1633db1c7f55778a9f442
busitaro <busitaro10@gmail.com>
2021-01-09 08:26:01 +0900

    input.pyのファイルスキップ条件を修正

--- input.py
+++ input.py
@@ -13,16 +13,17 @@ def input_data(file_path, exclude=True):
         with open(exclude_file, 'r') as f:
             exclude_list = [code.replace('\n', '') for code in f.readlines()]
     for file in os.listdir(file_path):
-        if exclude:
-            if re.sub('price_|.csv', '', file) in exclude_list:
-                continue
-        data[file] = \
-            pd.read_csv(
-                '{}/{}'.format(file_path, file), 
-                names=['date', 'begin', 'high', 'low', 'end', 'compare', 'compare_rate', 'turnover'],
-                parse_dates=['date'], 
-                index_col=['date'], 
-                date_parser=lambda date: datetime.datetime.strptime(date, '%y/%m/%d')).sort_index()
+        if file.startswith('price'):
+            if exclude:
+                if re.sub('price_|.csv', '', file) in exclude_list:
+                    continue
+            data[file] = \
+                pd.read_csv(
+                    '{}/{}'.format(file_path, file), 
+                    names=['date', 'begin', 'high', 'low', 'end', 'compare', 'compare_rate', 'turnover'],
+                    parse_dates=['date'], 
+                    index_col=['date'], 
+                    date_parser=lambda date: datetime.datetime.strptime(date, '%y/%m/%d')).sort_index()
     return data
 
 
