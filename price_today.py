634521b3bdf1eeea6b654ca5c66cd80668c9ed55
busitaro <busitaro10@gmail.com>
2021-03-12 22:39:35 +0900

    improve; price_today.pyについて、関数を細分化
    test性、保守性、機能性を鑑みて、関数分割

--- price_today.py
+++ price_today.py
@@ -15,21 +15,68 @@ def get_price_chart(soup):
     return list(map(lambda x: x.text, soup.select('#stock_kabuka_table tr')[1].find_all(['td', 'th'])))
 
 
-def main():
+def get_price_data(code: str) -> list: 
+    """
+    WEBから価格データを取得する
+
+    Params
+    --------
+    code: str
+        取得対象の銘柄コード
+
+    Returns
+    ---------
+    list:
+        取得した価格データ
+    """
+    soup = get_html(base_url.format(code=code))
+    if soup is not None:
+        data = get_price_chart(soup)
+        return data
+    else:
+        raise ValueError('soup is None (code: {})'.format(code))
+
+
+def output_price_data(code: str, data: list):
+    """
+    データをファイル出力する
+
+    Params
+    --------
+    code: str
+        銘柄コード
+    data: list
+        出力するデータ
+    """
     config = Config()
-    today = datetime.date.today().strftime('%Y%m%d')
+    path_to_output = '{}/{}'.format(config.output_path(), out_file.format(code=code))
+    with open(path_to_output, 'a', encoding='utf_8', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(data)
+
+
+def price_today(code: str):
+    """
+    指定された銘柄のデータ取得、出力を行う
+
+    Params
+    --------
+    code: str
+        銘柄コード
+    """
+    try:
+        data = get_price_data(code)
+        if len(data) > 0:
+            output_price_data(code, data)
+    except Exception as e:
+        print('type:{}'.format(str(type(e))))
+        print('args:' + str(e.args))
+
+
+def main():
     for code in range(1300, 10000):
         time.sleep(sleep_time / 1000)
-        soup = get_html(base_url.format(code=code))
-        try:
-            if soup is not None:
-                path_to_output = '{}/{}'.format(config.output_path(), out_file.format(code=code))
-                with open(path_to_output, 'a', encoding='utf_8', newline='') as f:
-                    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-                    writer.writerow(get_price_chart(soup))
-        except Exception as e:
-            print('type:{}'.format(str(type(e))))
-            print('args:' + str(e.args))
+        price_today(str(code))
 
 
 if __name__ == '__main__':
